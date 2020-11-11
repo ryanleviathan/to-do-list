1f9a472a0c9bc02afa66b2fed335dad76d425d84
ryanleviathan <ryan.m.carreras@gmail.com>
2020-11-10 18:36:34 -0800

    tests passing! MVO done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,31 +31,77 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('posts and gets Jons plants', async() => {
 
       const expectation = [
         {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
+          'id': 4,
+          'name': 'take out trash',
+          'priority_number': 9,
+          'is_completed': false,
+          'owner_id': 2
         },
         {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
+          'id': 5,
+          'name': 'take out recycling',
+          'priority_number': 6,
+          'is_completed': false,
+          'owner_id': 2
         },
         {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
+          'id': 6,
+          'name': 'unload dishwasher',
+          'priority_number': 5,
+          'is_completed': false,
+          'owner_id': 2
         }
       ];
 
+      await fakeRequest(app)
+        .post('/api/todos')
+        .send(expectation[0])
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      await fakeRequest(app)
+        .post('/api/todos')
+        .send(expectation[1])
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      await fakeRequest(app)
+        .post('/api/todos')
+        .send(expectation[2])
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/api/todos')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+        
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('updates todo bool to true', async() => {
+
+      const expectation =
+      [{
+        'id': 6,
+        'name': 'unload dishwasher',
+        'priority_number': 5,
+        'is_completed': true,
+        'owner_id': 2
+      }];
+
+      const data = await fakeRequest(app)
+        .put('/api/todos/6')
+        .set('Authorization', token)
+        .send(expectation)
         .expect('Content-Type', /json/)
         .expect(200);
 
