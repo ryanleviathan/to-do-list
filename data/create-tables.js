d30fec85fefa4fcb3fbade5842d4719b0bc61172
ryanleviathan <ryan.m.carreras@gmail.com>
2020-11-10 18:12:58 -0800

    verifyed routes through postman

--- data/create-tables.js
+++ data/create-tables.js
@@ -17,10 +17,11 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE todos (
                     id SERIAL PRIMARY KEY NOT NULL,
                     name VARCHAR(512) NOT NULL,
-                    cool_factor INTEGER NOT NULL,
+                    priority_number INTEGER NOT NULL,
+                    is_completed BOOLEAN NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
